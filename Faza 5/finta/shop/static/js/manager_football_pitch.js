fc13fc4d51501ff2bcf156ec1cb9e33e2bf45e06
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-05-21 12:57:12 +0200

    Sitne promene u manager-u

--- Faza 5/finta/shop/static/js/manager_football_pitch.js
+++ Faza 5/finta/shop/static/js/manager_football_pitch.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
 
     previous_item = ""
     previous_item_id = ""
+    manager_team = []
+    selected_position = ""
 
     inicijalizacija()
 
     function inicijalizacija() {
+        inicijalizujLocalStorage()
         inicijalizujTeren()
         dodajDogadjaje()
         ostalo()
@@ -14,6 +17,17 @@ $(document).ready(function() {
     function ostalo() {
         $("#position_name").html("(Select a position in order to search for a player)").css("color", "red")
         $("#player_search").attr("disabled", true)
+
+        if (selected_position !== "") {
+            $("#" + selected_position).css("background-color", "black")
+        }
+    }
+
+    function inicijalizujLocalStorage() {
+        if (localStorage.getItem("team") == null) localStorage.setItem("team", manager_team)
+        else manager_team = localStorage.getItem("team")
+        if (localStorage.getItem("selected_position") == null) localStorage.setItem("selected_position", selected_position)
+        else selected_position = localStorage.getItem("selected_position")
     }
 
     function dodajDogadjaje() {
@@ -36,6 +50,9 @@ $(document).ready(function() {
                 $("#player_search").attr("disabled", true)
             }
         }})
+        $("#player_search").on({change: function() {
+            console.log("oh my")
+        }})
     }
 
     function inicijalizujTeren() {
@@ -50,6 +67,7 @@ $(document).ready(function() {
             previous_item_id = ""
             $("#position_name").html("(Select a position in order to search for a player)").css("color", "red")
             $("#player_search").attr("disabled", true)
+            localStorage.setItem("selected_position", "")
         }
         else {
             if (previous_item_id !== "") {
@@ -58,6 +76,8 @@ $(document).ready(function() {
 
             previous_item = item
             previous_item_id = item.attr("id")
+            localStorage.setItem("selected_position", previous_item_id)
+
             $("#position_name").html("Search for a player at " + previous_item_id +
                 " position (team and player names, at least 4 letters each, no spaces):").css("color", "black")
             if ($("#player").val().length >= 4 && $("#player_team").val().length >= 4) {
