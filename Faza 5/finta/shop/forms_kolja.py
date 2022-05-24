ed11d243bd9cb837eb8b3d18fbb536e3d51b1f84
Konstantin Benovic <Konstantin Benovic>
2022-05-24 10:48:37 +0200

    Dodao sam komentare na fajlove koje sam zavrsio

--- Faza 5/finta/shop/forms_kolja.py
+++ Faza 5/finta/shop/forms_kolja.py
@@ -1,14 +1,8 @@
 from django.forms import Form, ModelForm
 from django import forms
 
+#Ceo ovaj fajl je napravio Konstantin Benovic 0114/2019
 
+#Klasa (forma) koja sluzi za pretragu timova po imenu
 class SearchForm(Form):
     Search = forms.CharField(max_length=70)
-
-class TeamS:
-    name = ""
-    logo = ""
-
-    def __init__(self, n, l):
-        name = n
-        logo = l
